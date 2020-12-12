bc389faa3726ff2225594660861044d2ca98bb94
Ornella <ornee.sci@gmail.com>
2020-12-12 14:12:19 -0300

    select

--- mujeres2000/src/Componentes/Select/Select.js
+++ mujeres2000/src/Componentes/Select/Select.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Select.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown} from '@fortawesome/free-solid-svg-icons';
 const Select = () => { 
     return(
         <div className='select-container'>
-            <label for="start" className='label-text'>Seleccione un Mes:
-                <input type="month" id="start" name="start" min="2020-01" value="2020-12"></input>
-            </label>
+            <h3 className='select-h3'>Seleccione mes y año</h3>
+            <FontAwesomeIcon icon={faChevronDown} className='faBars' / >
+            <div>
+                <label for="start">
+                    <input type="month" id="start" name="start" min="2020-01" value="2020-12"></input>
+                </label>
+            </div>
         </div>
 
     )
