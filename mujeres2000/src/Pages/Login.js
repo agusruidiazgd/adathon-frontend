909fb02449a7f38d3f12bda8d3598dbe0b96460c
Agustina Nahir Ruidiaz <agusruidiaz@hotmail.com>
2020-12-12 10:21:45 -0300

    first commit

---
+++ mujeres2000/src/Pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () =>{
+    return(
+        <section className="login">
+            <img src="" alt="Mujeres2000"/>
+            <p>Control de costos y ventas</p>
+            <form>
+                <input type="text" placeholder="E-mail"></input>
+                <input type="text" placeholder=""></input>
+            </form>
+        </section>
+    )
+}
+
+export default Login;
